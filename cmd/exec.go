a1f20730349af02c3d94da4a512cf25882da73b5
Jonas Riedel <jonasriedel@pm.me>
2023-12-12 14:24:16 +0100

    feat: add exec command

---
+++ cmd/exec.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
+)
+
+var execCmd = &cobra.Command{
+	Use:   "exec [containerID] [command]",
+	Short: "exec a command in a running container",
+	Args:  cobra.ExactArgs(2),
+	Run:   execCommand,
+}
+
+func execCommand(cmd *cobra.Command, args []string) {
+	containerID := args[0]
+	command := args[1:]
+
+	rootFolder := fmt.Sprintf("/var/lib/container-runtime/%s", containerID)
+	must(os.Chdir(rootFolder))
+
+	execCmd := exec.Command(command[0], command[1:]...)
+	execCmd.Stdin = os.Stdin
+	execCmd.Stdout = os.Stdout
+	execCmd.Stderr = os.Stderr
+
+	if err := execCmd.Run(); err != nil {
+		fmt.Println("ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(execCmd)
+}
