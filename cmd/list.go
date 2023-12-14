a4500769a8abb9ee96ebe38e8f4902aa553feb82
Jonas Riedel <jonasriedel@pm.me>
2023-12-14 15:40:56 +0100

    feat: filter containers by valid SHA-256 hashes

--- cmd/list.go
+++ cmd/list.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/spf13/cobra"
 )
@@ -20,23 +21,30 @@ func list(_ *cobra.Command, _ []string) error {
 	if err != nil {
 		return fmt.Errorf("error reading root path: %v", err)
 	}
-	var containers []string
+
+	var validContainers []string
+
 	for _, container := range containerDir {
-		if container.IsDir() {
-			containers = append(containers, container.Name())
+		if container.IsDir() && isSHA256Hash(container.Name()) {
+			validContainers = append(validContainers, container.Name())
 		}
 	}
 
-	if len(containers) == 0 {
+	if len(validContainers) == 0 {
 		fmt.Println("No containers are running")
 		return nil
 	}
 
 	// print out containers
-	for _, container := range containers {
+	for _, container := range validContainers {
 		fmt.Println(container)
 	}
 	return nil
+}
+
+// isSHA256Hash checks if a string is a valid SHA256 hash
+func isSHA256Hash(hash string) bool {
+	return regexp.MustCompile(`^[a-f0-9]{64}$`).MatchString(hash)
 }
 
 func init() {
